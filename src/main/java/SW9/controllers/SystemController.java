a5c63d5504ecb9bc5668124c623b81802f25b874
tgunde13 <tgunde13@student.aau.dk>
2017-12-05 11:20:06 +0100

    System view contex menu

--- src/main/java/SW9/controllers/SystemController.java
+++ src/main/java/SW9/controllers/SystemController.java
@@ -2,32 +2,77 @@ package SW9.controllers;
 
 import SW9.abstractions.HighLevelModelObject;
 import SW9.abstractions.SystemModel;
+import SW9.presentations.DropDownMenu;
+import com.jfoenix.controls.JFXPopup;
+import javafx.beans.property.ObjectProperty;
+import javafx.beans.property.SimpleObjectProperty;
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
 import javafx.scene.input.MouseEvent;
+import javafx.scene.shape.Circle;
 import javafx.scene.shape.Line;
+
+import java.net.URL;
+import java.util.ResourceBundle;
 
 /**
  * Controller for a system.
  */
-public class SystemController extends ModelController {
-    private SystemModel system;
+public class SystemController extends ModelController implements Initializable {
+    private final ObjectProperty<SystemModel> system;
     public Line topRightLine;
 
-    public SystemModel getSystem() {
-        return system;
+    private Circle dropDownMenuHelperCircle;
+    private DropDownMenu contextMenu;
+
+    public SystemController() {
+        system = new SimpleObjectProperty<>();
     }
 
-    public void setSystem(SystemModel system) {
-        this.system = system;
+    @Override
+    public void initialize(final URL location, final ResourceBundle resources) {
+        system.addListener((observable, oldValue, newValue) -> {
+            initializeSystemContextMenu();
+        });
+    }
+
+    public SystemModel getSystem() {
+        return system.get();
+    }
+
+    public void setSystem(final SystemModel system) {
+        this.system.setValue(system);
     }
 
     @FXML
     private void modelContainerPressed(final MouseEvent event) {
+        event.consume();
+        CanvasController.leaveTextAreas();
 
+        if (event.isSecondaryButtonDown()) {
+            dropDownMenuHelperCircle.setLayoutX(event.getX());
+            dropDownMenuHelperCircle.setLayoutY(event.getY());
+            DropDownMenu.x = event.getX();
+            DropDownMenu.y = event.getY();
+
+            contextMenu.show(JFXPopup.PopupVPosition.TOP, JFXPopup.PopupHPosition.LEFT, 0, 0);
+        }
     }
 
     @Override
     public HighLevelModelObject getModel() {
-        return system;
+        return getSystem();
+    }
+
+    private void initializeSystemContextMenu() {
+        dropDownMenuHelperCircle = new Circle(5);
+        dropDownMenuHelperCircle.setOpacity(0);
+        dropDownMenuHelperCircle.setMouseTransparent(true);
+
+        root.getChildren().add(dropDownMenuHelperCircle);
+
+        contextMenu = new DropDownMenu(root, dropDownMenuHelperCircle, 230, true);
+
+        contextMenu.addColorPicker(system.get(), system.get()::dye);
     }
 }
