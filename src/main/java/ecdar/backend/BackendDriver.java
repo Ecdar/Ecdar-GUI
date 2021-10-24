d584d48a0fbe998e0f04144ee088641e719504e2
Niels Vistisen <nielswps@gmail.com>
2021-10-24 10:47:10 +0200

    WIP: gRPC communication implementation

--- src/main/java/ecdar/backend/BackendDriver.java
+++ src/main/java/ecdar/backend/BackendDriver.java
@@ -1,8 +1,13 @@
 package ecdar.backend;
 
+import EcdarProtoBuf.ComponentProtos;
+import EcdarProtoBuf.EcdarBackendGrpc;
+import EcdarProtoBuf.QueryProtos;
 import ecdar.Ecdar;
+import ecdar.abstractions.Component;
 import ecdar.abstractions.QueryState;
-import ecdar.utility.protobuf.ProtoBufConverter;
+import io.grpc.ManagedChannel;
+import io.grpc.ManagedChannelBuilder;
 import javafx.util.Pair;
 
 import java.io.*;
@@ -23,7 +28,7 @@ public class BackendDriver {
 
     private final Queue<ExecutableQuery> waitingQueries = new LinkedList<>();
 
-    private Integer portNum = 5162;
+    private Integer globalPortNumber = 5162;
 
     private final ArrayList<Process> backendProcesses = new ArrayList<>();
 
@@ -31,22 +36,22 @@ public class BackendDriver {
         new Thread(() -> {
             while(true) {
                 // Try to read from all Reveaal instances that are executing a query and have outputted something
-                for (QuerySocket socket : reveaalSockets) {
-                    try {
-                        if (socket.isRunningQuery() && socket.getReader().ready()) readFromSocket(socket);
-                    } catch (IOException e) {
-                        e.printStackTrace();
-                    }
-                }
-
-                // Try to read from all jEcdar instances that are executing a query and have outputted something
-                for (QuerySocket socket : jEcdarSockets) {
-                    try {
-                        if (socket.isRunningQuery() && socket.getReader().ready()) readFromSocket(socket);
-                    } catch (IOException e) {
-                        e.printStackTrace();
-                    }
-                }
+//                for (QuerySocket socket : reveaalSockets) {
+//                    try {
+//                        if (socket.isRunningQuery() && socket.getReader().ready()) readFromSocket(socket);
+//                    } catch (IOException e) {
+//                        e.printStackTrace();
+//                    }
+//                }
+//
+//                // Try to read from all jEcdar instances that are executing a query and have outputted something
+//                for (QuerySocket socket : jEcdarSockets) {
+//                    try {
+//                        if (socket.isRunningQuery() && socket.getReader().ready()) readFromSocket(socket);
+//                    } catch (IOException e) {
+//                        e.printStackTrace();
+//                    }
+//                }
 
                 // Execute the next waiting query
                 ExecutableQuery query = waitingQueries.poll();
@@ -68,45 +73,45 @@ public class BackendDriver {
     }
 
     public Pair<ArrayList<String>, ArrayList<String>> getInputOutputs(String query) {
-        if (!query.startsWith("refinement")) {
-            return null;
-        }
-
-        // Pair is used as a tuple, not a key-value pair
-        Pair<ArrayList<String>, ArrayList<String>> inputOutputs = new Pair<>(new ArrayList<>(), new ArrayList<>());
-
-        ProcessBuilder pb = new ProcessBuilder("src/Reveaal", "-c", Ecdar.projectDirectory.get(), query.replaceAll("\\s", ""));
-        pb.redirectErrorStream(true);
-        try {
-            //Start the j-Ecdar process
-            Process ReveaalEngineInstance = pb.start();
-
-            //Communicate with the j-Ecdar process
-            try (
-                    var ReveaalReader = new BufferedReader(new InputStreamReader(ReveaalEngineInstance.getInputStream()));
-            ) {
-                //Read the result of the query from the j-Ecdar process
-                String line;
-                while ((line = ReveaalReader.readLine()) != null) {
-                    // Process the query result
-                    if (line.endsWith("extra inputs")){
-                        Matcher m = Pattern.compile("[\"]([^\"]+)[\"]").matcher(line);
-                        while(m.find()){
-                            inputOutputs.getKey().add(m.group(1));
-                        }
-                    } else if (line.startsWith("extra outputs")) {
-                        Matcher m = Pattern.compile("[\"]([^\"]+)[\"]").matcher(line);
-                        while(m.find()){
-                            inputOutputs.getValue().add(m.group(1));
-                        }
-                    }
-                }
-            }
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-
-        return inputOutputs;
+//        if (!query.startsWith("refinement")) {
+//            return null;
+//        }
+//
+//        // Pair is used as a tuple, not a key-value pair
+//        Pair<ArrayList<String>, ArrayList<String>> inputOutputs = new Pair<>(new ArrayList<>(), new ArrayList<>());
+//
+//        ProcessBuilder pb = new ProcessBuilder("src/Reveaal", "-c", Ecdar.projectDirectory.get(), query.replaceAll("\\s", ""));
+//        pb.redirectErrorStream(true);
+//        try {
+//            //Start the j-Ecdar process
+//            Process ReveaalEngineInstance = pb.start();
+//
+//            //Communicate with the j-Ecdar process
+//            try (
+//                    var ReveaalReader = new BufferedReader(new InputStreamReader(ReveaalEngineInstance.getInputStream()));
+//            ) {
+//                //Read the result of the query from the j-Ecdar process
+//                String line;
+//                while ((line = ReveaalReader.readLine()) != null) {
+//                    // Process the query result
+//                    if (line.endsWith("extra inputs")){
+//                        Matcher m = Pattern.compile("[\"]([^\"]+)[\"]").matcher(line);
+//                        while(m.find()){
+//                            inputOutputs.getKey().add(m.group(1));
+//                        }
+//                    } else if (line.startsWith("extra outputs")) {
+//                        Matcher m = Pattern.compile("[\"]([^\"]+)[\"]").matcher(line);
+//                        while(m.find()){
+//                            inputOutputs.getValue().add(m.group(1));
+//                        }
+//                    }
+//                }
+//            }
+//        } catch (IOException e) {
+//            e.printStackTrace();
+//        }
+//
+        return null;//inputOutputs;
     }
 
     public void closeAllSockets() throws IOException {
@@ -127,12 +132,12 @@ public class BackendDriver {
     }
 
     private void readFromSocket(QuerySocket socket) throws IOException {
-        int line;
+        String line;
         System.out.println("Starting read");
-        while (socket.getReader().ready() && (line = socket.getReader().read()) != 0) {
+        while (socket.getReader().ready() && !(line = socket.getReader().readLine()).isEmpty()) {
             System.out.println(line);
             // Skip the returned query and possible notes, as these should not be shown in the GUI
-            /*if (line.startsWith("note:")) {
+            if (line.startsWith("note:")) {
                 continue;
             }
 
@@ -146,7 +151,7 @@ public class BackendDriver {
             } else {
                 socket.getExecutableQuery().queryListener.getQuery().setQueryState(QueryState.SYNTAX_ERROR);
                 socket.getExecutableQuery().failure.accept(new BackendException.QueryErrorException(line));
-            }*/
+            }
 
             // ToDo NIELS: Maybe just trigger poll() on query queue here instead of while loop (would require some initial poll technique)
         }
@@ -184,13 +189,13 @@ public class BackendDriver {
         if (numberOfSockets.getKey().get() < numberOfSockets.getValue().get()) {
             try {
                 while (true) {
-                    portNum += 1;
+                    globalPortNumber += 1;
 
                     ProcessBuilder pb;
                     if (backend.equals(BackendHelper.BackendNames.jEcdar)) {
                         pb = new ProcessBuilder("java", "-jar", "src/libs/j-Ecdar.jar");
                     } else {
-                        pb = new ProcessBuilder("src/Reveaal", "-p", "127.0.0.1:" + portNum).inheritIO();
+                        pb = new ProcessBuilder("src/Reveaal", "-p", "127.0.0.1:" + globalPortNumber).inheritIO();
                     }
 
                     Process p = pb.start();
@@ -201,7 +206,7 @@ public class BackendDriver {
                     }
                 }
 
-                QuerySocket newSocket = new QuerySocket(new Socket((String) null, portNum, null, 0));
+                QuerySocket newSocket = new QuerySocket(new Socket((String) null, globalPortNumber, null, 0));
 
                 querySockets.add(newSocket);
                 numberOfSockets.getKey().getAndIncrement();
@@ -248,6 +253,8 @@ public class BackendDriver {
             this.reader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
             this.writer = new BufferedWriter(new OutputStreamWriter(socket.getOutputStream()));
             this.socket = socket;
+
+            socket.close();
         }
 
         public BufferedReader getReader() {
@@ -264,12 +271,24 @@ public class BackendDriver {
 
         public void executeQuery(ExecutableQuery executableQuery) {
             this.executableQuery = executableQuery;
-            try {
-                getWriter().write(ProtoBufConverter.getQueryProtoBuf(executableQuery.query).getQueryBytes().toStringUtf8().toCharArray());
-            } catch (IOException e) {
-                e.printStackTrace();
-                executableQuery.failure.accept(new BackendException.BadBackendQueryException(e.getMessage()));
+
+            ManagedChannel channel = ManagedChannelBuilder.forTarget(socket.getInetAddress().toString() + ":" + socket.getPort()).usePlaintext().build();
+
+            System.out.println("Channel: " + channel);
+
+            EcdarBackendGrpc.EcdarBackendBlockingStub stub = EcdarBackendGrpc.newBlockingStub(channel);
+
+            var componentsBuilder = QueryProtos.ComponentsUpdateRequest.newBuilder();
+
+            for (Component c : Ecdar.getProject().getComponents()) {
+                componentsBuilder.addComponents(ComponentProtos.Component.newBuilder().setJson(c.serialize().toString()).build());
             }
+
+            System.out.println(stub.updateComponents(componentsBuilder.build()));
+
+            QueryProtos.QueryResponse result = stub.sendQuery(QueryProtos.Query.newBuilder().setId(0).setQuery(executableQuery.query).build());
+
+            System.out.println(result.getResultCase().getNumber());
         }
 
         public boolean isRunningQuery() {
