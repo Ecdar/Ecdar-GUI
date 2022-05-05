f1dd1a5b929e12b94ecac094bc1c634ee55d4d32
Niels Vistisen <nielswps@gmail.com>
2022-05-05 12:28:07 +0200

    Version number updated

--- src/main/java/ecdar/Ecdar.java
+++ src/main/java/ecdar/Ecdar.java
@@ -41,7 +41,7 @@ import java.security.CodeSource;
 public class Ecdar extends Application {
     public static Preferences preferences = Preferences.userRoot().node("ECDAR");
     public static BooleanProperty autoScalingEnabled = new SimpleBooleanProperty(false);
-    public static final String VERSION = "2.1";
+    public static final String VERSION = "2.3.0-beta";
     public static boolean serializationDone = false;
     public static SimpleStringProperty projectDirectory = new SimpleStringProperty();
 
