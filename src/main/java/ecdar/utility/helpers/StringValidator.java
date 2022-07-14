87702e19a7714ec58f5888df3f1f7c52cda2f9a1
Niels Vistisen <nielswps@gmail.com>
2022-07-14 13:46:55 +0200

    Extra newline removed

--- src/main/java/ecdar/utility/helpers/StringValidator.java
+++ src/main/java/ecdar/utility/helpers/StringValidator.java
@@ -3,8 +3,7 @@ package ecdar.utility.helpers;
 public class StringValidator {
     public static boolean validateQuery(String input) {
         int openingParentheses = 0, closingParentheses = 0;
-
-
+        
         for (char c : input.toCharArray()) {
             if (c == '(') openingParentheses++;
             else if (c == ')') closingParentheses++;
