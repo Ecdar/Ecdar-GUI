b1d88fe6b5410d354f945a7a0310855f66c29c4c
Niels Vistisen <nielswps@gmail.com>
2022-07-20 11:51:22 +0200

    WIP: TestFXBase updated to fix TimeoutException

--- src/test/java/ecdar/ui/TestFXBase.java
+++ src/test/java/ecdar/ui/TestFXBase.java
@@ -1,9 +1,11 @@
 package ecdar.ui;
 
+import com.google.common.util.concurrent.SettableFuture;
 import ecdar.Ecdar;
 import javafx.scene.input.KeyCode;
 import javafx.scene.input.MouseButton;
 import javafx.stage.Stage;
+import javafx.stage.StageStyle;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.testfx.api.FxToolkit;
@@ -12,14 +14,17 @@ import org.testfx.framework.junit.ApplicationTest;
 import java.util.concurrent.TimeoutException;
 
 public class TestFXBase extends ApplicationTest {
+    public static SettableFuture<Stage> primaryStageFuture = SettableFuture.create();
+
     @BeforeEach
     public void setUp() throws Exception {
         ApplicationTest.launch(Ecdar.class);
     }
 
     @Override
-    public void start(Stage stage) {
-        stage.show();
+    public void start(Stage primaryStage) throws Exception {
+        primaryStage.initStyle(StageStyle.UNDECORATED);
+        primaryStageFuture.set(primaryStage);
     }
 
     @AfterEach
