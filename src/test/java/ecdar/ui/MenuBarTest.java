6c1e923a617e9cc7eb1e10c81de2d1d635e8c47c
Niels Vistisen <nielswps@gmail.com>
2022-07-27 11:11:04 +0200

    WIP: Present check removed from menu test

--- src/test/java/ecdar/ui/MenuBarTest.java
+++ src/test/java/ecdar/ui/MenuBarTest.java
@@ -1,7 +1,6 @@
 package ecdar.ui;
 
 import org.junit.jupiter.api.Test;
-import org.testfx.util.NodeQueryUtils;
 import org.testfx.util.WaitForAsyncUtils;
 
 import static org.testfx.api.FxAssert.verifyThat;
@@ -10,8 +9,7 @@ public class MenuBarTest extends TestFXBase {
     @Test
     public void gridMenuItemHidesGrid() {
         clickOn("View");
-        WaitForAsyncUtils.waitForFxEvents(2);
-        assert lookup("#menuBarViewGrid").match(NodeQueryUtils.isVisible()).tryQuery().isPresent();
+        WaitForAsyncUtils.waitForFxEvents(3);
         clickOn("#menuBarViewGrid");
 
         verifyThat(lookup("#grid"), g -> g.getOpacity() == 0);
