c6b8485ccd9eb97cf843a69c6a01927abc696625
Emilie Steinmann <est@ampler.io>
2022-11-04 11:57:19 +0100

    Parameterized test

--- src/test/java/ecdar/utility/StringHelperTest.java
+++ src/test/java/ecdar/utility/StringHelperTest.java
@@ -3,19 +3,26 @@ package ecdar.utility;
 import ecdar.abstractions.Query;
 import ecdar.abstractions.QueryState;
 import ecdar.utility.helpers.StringHelper;
+import org.junit.Before;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.junit.platform.commons.util.StringUtils;
+import org.junit.runner.RunWith;
 import org.junit.runners.Parameterized;
 
-public class StringHelperTest {
+import java.util.Arrays;
+import java.util.Collection;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class StringHelperTest {
     @Test
     public void ConvertSymbolsToUnicodeTest() {
         String stringToReplace = "2 >= 4";
         String result = StringHelper.ConvertSymbolsToUnicode(stringToReplace);
         String expected = "2 \u2265 4";
 
-        Assertions.assertEquals(expected, result);
+        assertEquals(expected, result);
     }
 }
+
