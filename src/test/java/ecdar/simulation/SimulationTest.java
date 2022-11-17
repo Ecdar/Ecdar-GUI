639d24b951ed03fa65a74e15f1ef20a12bd53480
APaludan <andreashp@outlook.com>
2022-11-17 12:43:12 +0100

    fix test fr

--- src/test/java/ecdar/simulation/SimulationTest.java
+++ src/test/java/ecdar/simulation/SimulationTest.java
@@ -3,12 +3,14 @@ package ecdar.simulation;
 import EcdarProtoBuf.EcdarBackendGrpc;
 import EcdarProtoBuf.ObjectProtos;
 import EcdarProtoBuf.QueryProtos;
+import EcdarProtoBuf.ObjectProtos.DecisionPoint;
+import EcdarProtoBuf.ObjectProtos.LocationTuple;
+import EcdarProtoBuf.ObjectProtos.SpecificComponent;
 import ecdar.TestFXBase;
 import ecdar.abstractions.Component;
 import ecdar.abstractions.Location;
 import io.grpc.BindableService;
 import io.grpc.ManagedChannel;
-import io.grpc.Server;
 import io.grpc.Status;
 import io.grpc.inprocess.InProcessChannelBuilder;
 import io.grpc.inprocess.InProcessServerBuilder;
@@ -21,69 +23,78 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
+import org.junit.jupiter.api.TestInstance.Lifecycle;
 import org.junit.jupiter.api.Assertions;
 
+@TestInstance(Lifecycle.PER_CLASS)
 public class SimulationTest extends TestFXBase {
     public GrpcCleanupRule grpcCleanup = new GrpcCleanupRule();
     private final String serverName = InProcessServerBuilder.generateName();
 
-    // @Test
-    // public void testGetInitialStateHighlightsTheInitialLocation() {
-    //     final List<Component> components = generateComponentsWithInitialLocations();
+    @Test
+    public void testGetInitialStateHighlightsTheInitialLocation() {
+        final List<Component> components = generateComponentsWithInitialLocations();
 
-    //     BindableService testService = new EcdarBackendGrpc.EcdarBackendImplBase() {
-    //         @Override
-    //         public void startSimulation(QueryProtos.SimulationStartRequest request,
-    //                                     StreamObserver<QueryProtos.SimulationStepResponse> responseObserver) {
-    //             try {
-    //                 ObjectProtos.State state = ObjectProtos.StateTuple.newBuilder().addAllLocations(components.stream()
-    //                         .map(c -> ObjectProtos.StateTuple.LocationTuple.newBuilder()
-    //                                 .setComponentName(c.getName())
-    //                                 .setId(c.getInitialLocation().getId())
-    //                                 .build())
-    //                         .collect(Collectors.toList())).build();
+        BindableService testService = new EcdarBackendGrpc.EcdarBackendImplBase() {
+            @Override
+            public void startSimulation(QueryProtos.SimulationStartRequest request,
+                                        StreamObserver<QueryProtos.SimulationStepResponse> responseObserver) {
+                try {
+                    ObjectProtos.LocationTuple locations = LocationTuple.newBuilder().addAllLocations(components.stream()
+                            .map(c -> ObjectProtos.Location.newBuilder()
+                            .setSpecificComponent(SpecificComponent.newBuilder()
+                                    .setComponentName(c.getName()))
+                            .setId(c.getInitialLocation().getId())
+                            .build())
+                            .collect(Collectors.toList()))
+                        .build();
+                    
+                    ObjectProtos.State state = ObjectProtos.State.newBuilder().setLocationTuple(locations).build();
+                    DecisionPoint decisionPoint = DecisionPoint.newBuilder().setSource(state).build();
+                    QueryProtos.SimulationStepResponse response = QueryProtos.SimulationStepResponse.newBuilder()
+                            .setNewDecisionPoint(decisionPoint)
+                            .build();
+                    responseObserver.onNext(response);
+                    responseObserver.onCompleted();
+                } catch (Throwable e) {
+                    responseObserver.onError(Status.INVALID_ARGUMENT.withDescription(e.getMessage()).asException());
+                }
+            }
 
-    //                 QueryProtos.SimulationStepResponse response = QueryProtos.SimulationStepResponse.newBuilder().setState(state).build();
-    //                 responseObserver.onNext(response);
-    //                 responseObserver.onCompleted();
-    //             } catch (Throwable e) {
-    //                 responseObserver.onError(Status.INVALID_ARGUMENT.withDescription(e.getMessage()).asException());
-    //             }
-    //         }
+            @Override
+            public void takeSimulationStep(EcdarProtoBuf.QueryProtos.SimulationStepRequest request,
+                                           io.grpc.stub.StreamObserver<EcdarProtoBuf.QueryProtos.SimulationStepResponse> responseObserver) {
+            }
+        };
 
-    //         @Override
-    //         public void takeSimulationStep(EcdarProtoBuf.QueryProtos.SimulationStepRequest request,
-    //                                        io.grpc.stub.StreamObserver<EcdarProtoBuf.QueryProtos.SimulationStepResponse> responseObserver) {
-    //         }
-    //     };
+        final ManagedChannel channel;
+        final EcdarBackendGrpc.EcdarBackendBlockingStub stub;
+        try {
+            grpcCleanup.register(InProcessServerBuilder
+                    .forName(serverName).directExecutor().addService(testService).build().start());
+            channel = grpcCleanup.register(InProcessChannelBuilder
+                    .forName(serverName).directExecutor().build());
+            stub = EcdarBackendGrpc.newBlockingStub(channel);
 
-    //     final Server server;
-    //     final ManagedChannel channel;
-    //     final EcdarBackendGrpc.EcdarBackendBlockingStub stub;
-    //     try {
-    //         server = grpcCleanup.register(InProcessServerBuilder
-    //                 .forName(serverName).directExecutor().addService(testService).build().start());
-    //         channel = grpcCleanup.register(InProcessChannelBuilder
-    //                 .forName(serverName).directExecutor().build());
-    //         stub = EcdarBackendGrpc.newBlockingStub(channel);
-    //         QueryProtos.SimulationStartRequest request = QueryProtos.SimulationStartRequest.newBuilder().setSystem("(A || B)").build();
+            QueryProtos.SimulationStartRequest request = QueryProtos.SimulationStartRequest.newBuilder().build();
 
-    //         var expectedResponse = new ObjectProtos.StateTuple.LocationTuple[components.size()];
+            var expectedResponse = new ObjectProtos.Location[components.size()];
 
-    //         for (int i = 0; i < components.size(); i++) {
-    //             Component comp = components.get(i);
-    //             expectedResponse[i] = ObjectProtos.StateTuple.LocationTuple.newBuilder()
-    //                     .setComponentName(comp.getName())
-    //                     .setId(comp.getInitialLocation().getId()).build();
-    //         }
+            for (int i = 0; i < components.size(); i++) {
+                Component comp = components.get(i);
+                expectedResponse[i] = ObjectProtos.Location.newBuilder()
+                        .setSpecificComponent(SpecificComponent.newBuilder().setComponentName(comp.getName()))
+                        .setId(comp.getInitialLocation().getId()).build();
+            }
 
-    //         var result = stub.startSimulation(request).getState().getLocationsList().toArray();
+            var result = stub.startSimulation(request).getNewDecisionPoint().getSource().getLocationTuple().getLocationsList().toArray();
 
-    //         Assertions.assertArrayEquals(expectedResponse, result);
-    //     } catch (IOException e) {
-    //         Assertions.fail("Exception encountered: " + e.getMessage());
-    //     }
-    // }
+            Assertions.assertArrayEquals(expectedResponse, result);
+        } catch (IOException e) {
+            Assertions.fail("Exception encountered: " + e.getMessage());
+        }
+    }
 
     private List<Component> generateComponentsWithInitialLocations() {
         List<Component> comps = new ArrayList<>();
