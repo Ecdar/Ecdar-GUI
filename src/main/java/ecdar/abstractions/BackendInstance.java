e2a187301b5ffa314cf36cb82187f1eb01191f40
Casper-NS <caspernyvang@gmail.com>
2022-12-01 14:27:58 +0100

    renamed a parameter

--- src/main/java/ecdar/abstractions/BackendInstance.java
+++ src/main/java/ecdar/abstractions/BackendInstance.java
@@ -57,8 +57,8 @@ public class BackendInstance implements Serializable {
         return isThreadSafe;
     }
 
-    public void setIsThreadSafe(boolean aThreadSafe) {
-        isThreadSafe = aThreadSafe;
+    public void setIsThreadSafe(boolean threadSafe) {
+        isThreadSafe = threadSafe;
     }
 
     public String getBackendLocation() {
