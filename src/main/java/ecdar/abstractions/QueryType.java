66c5fd4f779a8b244db109d8b16562c6dd6fe5d7
Niels F. S. Vistisen <42961494+Nielswps@users.noreply.github.com>
2023-03-01 20:43:05 +0100

    Query types updated to follow theory (#143)
    * Query types updated to follow theory
    
    * Updated bisim_minim name
    
    * Fix typo
    
    ---------
    
    Co-authored-by: Sebastian Lund <sebastianlund112@gmail.com>

--- src/main/java/ecdar/abstractions/QueryType.java
+++ src/main/java/ecdar/abstractions/QueryType.java
@@ -3,13 +3,12 @@ package ecdar.abstractions;
 public enum QueryType {
     REACHABILITY("reachability", "E<>"),
     REFINEMENT("refinement", "<="),
-    QUOTIENT("quotient", "\\"),
     SPECIFICATION("specification", "Spec"),
     IMPLEMENTATION("implementation", "Imp"),
-    LOCAL_CONSISTENCY("consistency", "lCon"), // ToDo NIELS: Will become local-consistency
-    GLOBAL_CONSISTENCY("global-consistency", "gCon"),
-    BISIM_MIN("bisim", "bsim"),
-    GET_COMPONENT("get-component", "get");
+    CONSISTENCY("consistency", "Con"),
+    LOCAL_CONSISTENCY("local-consistency", "LCon"),
+    BISIM_MINIM("bisim-minim", "Bsim"),
+    GET_COMPONENT("get-component", "Get");
 
     private final String queryName;
     private final String symbol;
@@ -29,26 +28,23 @@ public enum QueryType {
 
     public static QueryType fromString(String s) {
         switch (s.toLowerCase()) {
+            case "reachability":
+                return REACHABILITY;
             case "refinement":
                 return REFINEMENT;
-            case "quotient":
-                return QUOTIENT;
             case "specification":
                 return SPECIFICATION;
             case "implementation":
                 return IMPLEMENTATION;
             case "consistency":
+                return CONSISTENCY;
             case "local-consistency":
                 return LOCAL_CONSISTENCY;
-            case "global-consistency":
-                return GLOBAL_CONSISTENCY;
-            case "bisim":
-                return BISIM_MIN;
+            case "bisim-minim":
+                return BISIM_MINIM;
             case "get":
             case "get-component":
                 return GET_COMPONENT;
-            case "reachability":
-                return REACHABILITY;
             default:
                 return null;
         }
